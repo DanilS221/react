5545a4233c0e1f6868de9736b6b6ca9eb0ec6683
Danik <suecv4299@gmail.com>
2025-07-27 12:10:20 +0300

    css_module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () =>{
     return(
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://static.vecteezy.com/system/resources/previews/036/431/225/non_2x/ai-generated-high-quality-logo-style-silhouette-of-a-cherry-blossom-twigs-free-png.png'></img>
             {/* <h1>PopiCon</h1> */}
         </header>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
+import s from'./Profile.module.css';
 
 const Profile = () =>{
     return(
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Elegant_Background-17.jpg/1200px-Elegant_Background-17.jpg?20170710104936'/>
             </div>
 
             <div>ava + description</div>
 
-            <div>
+            <div className='posts'>
                 my post
-                <div>
+
+
+                <div className='create_post'>
                     Create new post
                 </div>
-                <div>
+                
+                <div className={s.item}>
                     1 post
                 </div>
-                <div>
+
+                <div className={s.item}>
                     2 post
                 </div>
+
             </div>
 
             <div></div>
