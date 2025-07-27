b57e12bf9a28f83414eaf253e7475c9185879e00
Danik <suecv4299@gmail.com>
2025-07-27 10:32:22 +0300

    First_commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <header className='header'>
+            <img src='https://static.vecteezy.com/system/resources/previews/036/431/225/non_2x/ai-generated-high-quality-logo-style-silhouette-of-a-cherry-blossom-twigs-free-png.png'></img>
+            {/* <h1>PopiCon</h1> */}
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () =>{
+    return(
+        <div className='content'>
+            <div>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Elegant_Background-17.jpg/1200px-Elegant_Background-17.jpg?20170710104936'/>
+            </div>
+
+            <div>ava + description</div>
+
+            <div>
+                my post
+                <div>
+                    Create new post
+                </div>
+                <div>
+                    1 post
+                </div>
+                <div>
+                    2 post
+                </div>
+            </div>
+
+            <div></div>
+        </div>
+    );
+}
+
+export default Profile;
